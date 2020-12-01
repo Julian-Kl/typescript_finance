59c93f83554fbf7584fbc41ed41e991a2ccb28d4
Julian-Kl <juliensworld@outlook.com>
2020-12-01 17:24:30 +0100

    Finished
    finished finance logger project

--- public/app.js
+++ public/app.js
@@ -1,31 +1,23 @@
-"use strict";
-// classes
-var Invoice = /** @class */ (function () {
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return '${this.client} owes ${this.amount}€ for ${this.details}';
-    };
-    return Invoice;
-}());
-var invOne = new Invoice('mario', 'work on the mario website', 250);
-var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-var invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.push(new Invoice('mario', 'work on the mario website', 250));
-invOne.client = 'yoshi';
-console.log(invoices);
-var form = document.querySelector('.new-item-form');
+import { Invoice } from './modules/Invoice.js';
+import { Payment } from './modules/Payment.js';
+import { ListTemplate } from './modules/ListTemplate.js';
+const form = document.querySelector('.new-item-form');
 // Inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,9 @@
-// classes
-class Invoice {
-    client: string;
-    details: string;
-    amount: number;
-
-    constructor(client: string, details: string, amount: number){
-        this.client = client;
-        this.details = details;
-        this.amount = amount
-    }
-
-    format() {
-        return '${this.client} owes ${this.amount}€ for ${this.details}'
-    }
-}
-
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne)
-invoices.push(invTwo)
-
-invoices.push(new Invoice('mario', 'work on the mario website', 250))
-
-invOne.client = 'yoshi'; 
-
-console.log(invoices)
+import { Invoice } from './modules/Invoice.js';
+import { Payment } from './modules/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
+import { ListTemplate } from './modules/ListTemplate.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
 
 // Inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -37,13 +11,20 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber ,
-    );
-})
+    let doc: HasFormatter;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    } else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    
+    list.render(doc, type.value, 'end');
+
+});
